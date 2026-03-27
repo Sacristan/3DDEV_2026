65a4856084e4405fb65c06ded9424a71434c0c7f
Ģirts Ķesteris <girts.kesteris@gmail.com>
2026-03-27 16:26:32 +0200

    add pickable trigger

---
+++ Assets/Src/Pickable.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log($"{nameof(Pickable)} START @ {gameObject.name}", gameObject);
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"{nameof(Pickable)} collided with {collision.gameObject.name}", collision.gameObject);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"{nameof(Pickable)} trigger zone entered by {other.gameObject.name}", other.gameObject);
+        Destroy(gameObject);
+    }
+
+}
