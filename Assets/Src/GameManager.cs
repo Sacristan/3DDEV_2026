85f89760efcc2b281bb006de96d9cfdecb7638bc
Ģirts Ķesteris <girts.kesteris@gmail.com>
2026-03-27 17:14:18 +0200

    pickable collection logic

---
+++ Assets/Src/GameManager.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private List<Pickable> uncollectedPickables;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    private void Start()
+    {
+        uncollectedPickables = new(FindObjectsByType<Pickable>(FindObjectsSortMode.None));
+    }
+
+    public void OnPickableCollected(Pickable pickable)
+    {
+        Debug.Log($"{nameof(OnPickableCollected)} {pickable.gameObject.name}");
+        uncollectedPickables.Remove(pickable);
+
+        if (uncollectedPickables.Count == 0)
+        {
+            Debug.Log("VICTORY");
+        }
+    }
+}
--- Assets/Src/Pickable.cs
+++ Assets/Src/Pickable.cs
@@ -3,30 +3,26 @@ using UnityEngine;
 
 public class Pickable : MonoBehaviour
 {
-    void Start()
-    {
-        Debug.Log($"{nameof(Pickable)} START @ {gameObject.name}", gameObject);
-    }
-    
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log($"{nameof(Pickable)} collided with {collision.gameObject.name}", collision.gameObject);
-    }
-    
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     Debug.Log($"{nameof(Pickable)} collided with {collision.gameObject.name}", collision.gameObject);
+    // }
+
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{nameof(Pickable)} trigger zone entered by {other.gameObject.name}", other.gameObject);
-
+        // Debug.Log($"{nameof(Pickable)} trigger zone entered by {other.gameObject.name}", other.gameObject);
         // if (other.gameObject.CompareTag("Player"))
-        // {
-        // }
-        
+
         if (other.gameObject.TryGetComponent<Player>(out var player))
         {
-            Destroy(gameObject);
+            PickedUpByPlayer();
         }
-        
-
     }
 
+    void PickedUpByPlayer()
+    {
+        // FindAnyObjectByType<GameManager>().OnPickableCollected(this);
+        GameManager.instance.OnPickableCollected(this);
+        Destroy(gameObject);
+    }
 }
