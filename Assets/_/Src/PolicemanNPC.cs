6890d6e5fb5a65299783b9279c16ee294ff3658f
Ģirts Ķesteris <girts.kesteris@gmail.com>
2026-04-24 15:47:29 +0300

    add pathfinding closeenough

--- Assets/_/Src/PolicemanNPC.cs
+++ Assets/_/Src/PolicemanNPC.cs
@@ -1,16 +1,32 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PolicemanNPC : MonoBehaviour
 {
+    [SerializeField] private float closeEnoughDistance = 0.5f;
+
     private NavMeshAgent _agent;
     private Player _player;
+
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _player = FindAnyObjectByType<Player>();
-        
-        _agent.SetDestination(_player.transform.position);
     }
 
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, _player.transform.position);
+
+        if (distance > closeEnoughDistance)
+        {
+            _agent.isStopped = false;
+            _agent.SetDestination(_player.transform.position);
+        }
+        else
+        {
+            _agent.isStopped = true;
+        }
+    }
 }
