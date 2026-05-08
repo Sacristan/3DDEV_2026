ba811948f1ad3122aa3486d05f0b684804c4609c
Ģirts Ķesteris <girts.kesteris@gmail.com>
2026-05-08 11:53:42 +0300

    add restart level

--- Assets/_/Src/GameManager.cs
+++ Assets/_/Src/GameManager.cs
@@ -1,11 +1,17 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     private List<Pickable> uncollectedPickables = new();
+
+    [SerializeField] private float gameWonRestartTimer = 3f;
+
+    private bool isGameWon = false;
 
     private void Awake()
     {
@@ -23,9 +29,23 @@ public class GameManager : MonoBehaviour
         Debug.Log($"{nameof(OnPickableCollected)} {pickable.gameObject.name}");
         uncollectedPickables.Remove(pickable);
 
-        if (uncollectedPickables.Count == 0)
-        {
-            Debug.Log("VICTORY");
-        }
+        if (uncollectedPickables.Count == 0) GameWon();
     }
+
+    void GameWon()
+    {
+        if (isGameWon) return;
+
+        isGameWon = true;
+        Debug.Log("VICTORY");
+        StartCoroutine(GameWonRoutine());
+    }
+
+    IEnumerator GameWonRoutine()
+    {
+        Debug.Log("Trigger Restart Started");
+        yield return new WaitForSeconds(gameWonRestartTimer);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
 }
