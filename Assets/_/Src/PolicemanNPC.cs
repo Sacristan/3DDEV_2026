29bb17dc0a1df84b96b421d28f7ff49de035849a
Ģirts Ķesteris <girts.kesteris@gmail.com>
2026-05-08 12:51:05 +0300

    state driven npc

--- Assets/_/Src/PolicemanNPC.cs
+++ Assets/_/Src/PolicemanNPC.cs
@@ -1,43 +1,75 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PolicemanNPC : MonoBehaviour
 {
-    public event System.Action OnReachedTarget; 
-    
+    public event System.Action OnReachedTarget;
+
     [SerializeField] private float closeEnoughDistance = 0.5f;
 
     private NavMeshAgent _agent;
     private Player _player;
     private Animator _animator;
 
-    void Start()
+    public enum PolicemanState
+    {
+        None,
+        FollowingTarget,
+        ReachedTarget
+    }
+
+    private PolicemanState _state = PolicemanState.None;
+
+    public PolicemanState CurrentState
+    {
+        get => _state;
+        set
+        {
+            if (_state != value)
+            {
+                _state = value;
+                UpdateState();
+            }
+        }
+    }
+
+    IEnumerator Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _player = FindAnyObjectByType<Player>();
         _animator = GetComponentInChildren<Animator>();
+
+        yield return new WaitForSeconds(3f);
+        CurrentState = PolicemanState.FollowingTarget;
     }
 
     private void Update()
     {
+        if (CurrentState == PolicemanState.None) return;
+
         float distance = Vector3.Distance(transform.position, _player.transform.position);
 
         if (distance > closeEnoughDistance)
         {
             _agent.SetDestination(_player.transform.position);
-            UpdateMovement(isMoving: true);
+            CurrentState = PolicemanState.FollowingTarget;
         }
         else
         {
-            ReachedTarget();
+            CurrentState = PolicemanState.ReachedTarget;
         }
     }
 
-    void ReachedTarget()
+    void UpdateState()
     {
-        UpdateMovement(isMoving: false);
-        OnReachedTarget?.Invoke();
+        UpdateMovement(isMoving: CurrentState == PolicemanState.FollowingTarget);
+
+        if (CurrentState == PolicemanState.ReachedTarget)
+        {
+            OnReachedTarget?.Invoke();
+        }
     }
 
     void UpdateMovement(bool isMoving)
